6adc6ad4e963b69f821805e38ce61ce7a5458679
mujthabamk2 <mujthabamk2@gmail.com>
2025-04-19 00:18:15 +0530

    recruter page updated for backend working

--- docs/assets/js/recruiter.js
+++ docs/assets/js/recruiter.js
@@ -1,16 +1,10 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwMpqDRw9KcdC2dYqGoxqmoNbmzjNTTYVtOYzxmmm9CP6dKyeGYHj2PY1iRmQ2F_iwz/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz-Q1Vkc8QXQHKvFSYJxduEpIT6x_dpwIiROhHb--yER-dflzXL77QjdibsFxscTDPU/exec';
 const form = document.forms['job-form'];
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-      alert("✅ Job submitted successfully!");
-      form.reset(); // Optionally reset the form
-    })
-    .catch(error => {
-      alert("❌ Something went wrong. Please try again.");
-      console.error('Error!', error.message);
-    });
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => alert("Thank you! Vacancy submitted successfully."))
+    .then(() => form.reset())
+    .catch(error => alert('Error occurred. Please try again.'));
 });
